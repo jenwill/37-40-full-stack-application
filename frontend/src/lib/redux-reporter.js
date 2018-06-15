1514f994cded02e380bd7a366fa63dc1f974e4a4
Jennifer Williams <jennifer@squareandplum.com>
2018-06-14 18:52:40 -0700

    fix linter error, remove unnecessary comment

--- frontend/src/lib/redux-reporter.js
+++ frontend/src/lib/redux-reporter.js
@@ -1,12 +1,3 @@
-
-// ES5 way of writing the following code:
-// export default function (store) {
-//   return function (next){
-//     return function (action) {
-//       // do logic here
-//     }
-//   }}
-
 export default store => next => (action) => {
   try {
     console.log('__ACTION__', action);
@@ -14,7 +5,6 @@ export default store => next => (action) => {
     console.log('__STATE__', store.getState());
     return result;
   } catch (error) {
-    // Vinicio - gracefully handle the error in a way a user will understand
     console.log('__ERROR__', error);
     return action;
   }
